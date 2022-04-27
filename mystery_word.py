fa7a5bbc16c28adba6d42aa27d8541e2403bade3
hjoyheath <hillaryjoyheath@gmail.com>
2022-04-27 16:50:48 -0400

    random words add on

--- mystery_word.py
+++ mystery_word.py
@@ -1,4 +1,7 @@
-#validates guess
+# validates guess
+import random
+
+
 def is_valid_guess(guess):
     if len(guess) == 1 & guess.isalpha():
         return True
@@ -20,48 +23,54 @@ def mask_word(word, guesses):
 
 
 def play_game(file):
-    with open(file, "r") as f:
+    with open('words.txt') as f:
         file_string = f.read().lower()
-        print(f"Word length is {len(file_string)}.")
+        file_list = file_string.split()
+        random_word = random.choice(file_list)
+        print(random_word)
+    
+        print(f"Word length is {len(random_word)}.")
         file_string = file_string.lower()
     # above makes uppercase letters for visibility purpose
     # below makes characters of word into list
-        word_list = list(file_string)
+        # word_list = file_string.split()
         # print(word_list)
     chance = 8
-    correct_guess = len(word_list)
-    guesses = []
+    correct_guess = len(random_word)
+    guesses = ' '
 
     while True:
-        print(mask_word(word_list, guesses))
         guess = input("Please guess a letter: ").lower()
-        print(guess)
-        # add guess to guesses list
         
+        # print(guess)
+        # add guess to guesses list  
         if guess in guesses:
             print(f"Whoops you already guessed {guess}.")
-        
-        elif guess in word_list:
+        elif guess in random_word:
+            print(guess, end=" ")
+            print()
             print("You guessed correctly.")
 
             correct_guess -= 1
-            guesses.append(guess)
+            guesses += guess
+            # guesses.append(guess)
             if correct_guess == 0:
-                print("Congratulations, you won, the word is:", file_string)
+                print("Congratulations, you won, the word is:", random_word)
                 break
-        elif guess not in word_list:
+        elif guess not in random_word:
             if guess in guesses:
                 print(f"Whoops you already guessed {guess}.")
             elif guess not in guesses:
+                # guesses.append(guess)
                 chance -= 1
                 print(f"Sorry, try again, you have {chance} chances left.")
             if chance == 0:
-                print("You have run out of guesses, the word is:", file_string)
+                print("You have run out of guesses, the word is:", random_word)
                 break
     # then check guess against word's letters
     # tell user if guess is in word
     # user can then guess again
 
 
-# if __name__ == "__main__":
-play_game("test-word.txt")
+if __name__ == "__main__":
+    play_game('words.txt')
